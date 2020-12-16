b638d678f8da9a7051281c423fa7dc79f9b6d911
Idzel Jaimes <irjaimes@outlook.com>
2020-12-16 11:06:24 -0800

    added if statement to for loop for welcome alert and round number

--- assets/js/game.js
+++ assets/js/game.js
@@ -52,20 +52,32 @@ var fight = function(enemyName) { //fight function expression
             alert(playerName + " still has " + playerHealth + " health left.");
             console.log(playerName + " still has " + playerHealth + " health left.");
         }
+        
             
     
      
     } //while loop end
     
 };//fight() function end
-    
 
 for (var i = 0; i< enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
-    //debugger;
-    //fight(enemyNames [i]); //each enemyName is passed as argument
-}
+    if (playerHealth > 0){
+        //let player know what round thery are in, remember that arrays start at 0 so it needs to have 1 added to it
+        alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        
+        //pick new enemy to fight based on the index of the enemyName array
+        var pickedEnemyName = enemyNames[i];
+        
+        //reset enemyHealth before startgin new fight
+        enemyHealth = 50;
 
-//if (enemyHealth > 0) {} //if the enemy robot has health points, CONTINUE to fight
+        //use debugger to pause script from running and check what's going on at that moment in the code
+        //debugger (<--commented out)
+
+        //pass the pickedName variable's value into the fight function, where it'll assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    }else { //this else conditional did NOT display. ask about it
+        alert("You have lost your robot in battle! Game Over!"); 
+        break;
+    }
+}
