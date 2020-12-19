ee0e904598aeb4d473408bf4959b0c0c88baabda
Idzel Jaimes <irjaimes@outlook.com>
2020-12-19 13:14:43 -0800

    added shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,11 @@
 // GLOBAL VARIABLES
 var playerName = prompt("What is your robot's name?");
-var playerHealth = 50;//change back to 100
+var playerHealth = 50;//***CHANGE back to 100
 var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = 30;//***CHANGE back to 50
 var enemyAttack = 12;
 
 // FIGHT FUNCTION
@@ -66,7 +66,7 @@ var fight = function(enemyName) { // function(enemyName) passes arbitrary enemyN
 //START GAME function
 var startGame = function() {
 // reset player stats
-playerHealth = 100;
+playerHealth = 50;///*****CHANGE BACK TO 100 */
 playerAttack = 10;
 playerMoney = 10;
       
@@ -80,7 +80,16 @@ for(var i = 0; i < enemyNames.length; i++) {
             enemyHealth = 50;
             //debugger; // to pause script from running, check each line ran
             fight(pickedEnemyName); //pass the pickedEnemyName variable VALUE into the fight() function where it will assume the VALUE of enemyName parameter
-        }else { 
+        }// if player is alive and we're not on the last enemy in the array. ensures shop() is called after every fight if loop iterator, i, can increment
+            if (playerHealth > 0 && i < enemyNames.length - 1) { //using length attribute & - 1 lets us access last item in array no matter how many items in array
+                var storeConfirm = confirm("The fight is over, visit the store before the next round?");
+                //if yes(confirm), take them to shop function
+                if (storeConfirm) {
+                    shop();
+                }
+                
+            }
+        else { 
             alert("You have lost your robot in battle! Game Over!"); 
             break;
         }   
@@ -107,6 +116,55 @@ var endGame = function() {
     alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 };
+
+var shop = function() {
+    // ask player what they'd like to do
+    var shopOptionPrompt = prompt(
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+    // use switch to carry out action
+    switch (shopOptionPrompt) {
+        case "REFILL": 
+        case "refill":
+            if (playerMoney >= 7) {
+                alert("Refilling player's health by 20 for 7 dollars.");
+
+                // increase health and decrease money
+                playerHealth = playerHealth + 20;
+                playerMoney = playerMoney - 7;
+            }
+            else {
+                alert("You don't have enough money!");
+            }
+            break;
+
+        case "UPGRADE":
+        case "upgrade":
+            if (playerMoney >= 7) {
+              alert("Upgrading player's attack by 6 for 7 dollars.");
+          
+             // increase attack and decrease money
+              playerAttack = playerAttack + 6;
+              playerMoney = playerMoney - 7;
+            }
+            else {
+              alert("You don't have enough money!");
+            }
+            break;
+        
+        case "LEAVE":
+        case "leave":
+        alert("Leaving the store.");
+
+        // do nothing, so function will end
+        break;
+        default:
+        alert("You did not pick a valid option. Try again.");
+    
+        // call shop() again to force player to pick a valid option
+        shop();
+        break;
+    }      
+};  
 
 
 // call start the game when the page loads
